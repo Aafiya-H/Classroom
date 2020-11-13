1fd3c057c9756c54c37fecd42b197e103075c98a
Aafiya <aafiya.hussain2@gmail.com>
2020-11-13 12:16:13 +0530

    database initialized

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+admin.site.register(Classrooms)
+admin.site.register(Students)
+admin.site.register(Teachers)
+admin.site.register(Assignments)
+admin.site.register(Submissions)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Classrooms(models.Model):
+    classroom_name=models.CharField(max_length=100)
+
+class Students(models.Model):
+    student_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    classroom_id=models.ForeignKey(Classrooms,on_delete=models.CASCADE)
+
+class Teachers(models.Model):
+    teacher_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    classroom_id=models.ForeignKey(Classrooms,on_delete=models.CASCADE) 
+
+class Assignments(models.Model):
+    assignment_name=models.CharField(max_length=50)
+    classroom_id=models.ForeignKey(Classrooms,on_delete=models.CASCADE)
+    due_date=models.DateField()
+    posted_date=models.DateField(auto_now_add=True)
+    instructions=models.TextField()
+    total_marks=models.IntegerField(default=100)
+
+class Submissions(models.Model):
+    assignment_id=models.ForeignKey(Assignments,on_delete=models.CASCADE)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    submitted_date=models.DateField(auto_now_add=True)
+    submitted_on_time=models.BooleanField()
+    marks_alloted=models.IntegerField(default=0)
+
