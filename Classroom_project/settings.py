5f0ad03b7d8b48cedf0145df6d9cd1bbe1b41330
Aafiya <aafiya.hussain2@gmail.com>
2021-01-14 23:04:32 +0530

    added to allowed_hosts

--- Classroom_project/settings.py
+++ Classroom_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['google-classroom-11-11-20.herokuapp.com']
 
 
 # Application definition
