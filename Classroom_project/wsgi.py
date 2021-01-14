527693ee07d560d67db359131b0177a683c777a1
Aafiya <aafiya.hussain2@gmail.com>
2021-01-14 23:00:25 +0530

    WhiteNoise issue resolved

--- Classroom_project/wsgi.py
+++ Classroom_project/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Classroom_project.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
