8cdc340c8adb5f3cc8b5b97ff0a42ccb267a0cea
Aafiya <aafiya.hussain2@gmail.com>
2021-01-14 20:23:19 +0530

    ran collectstatic command

--- Classroom_project/settings.py
+++ Classroom_project/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
