1a8dd22d927d9b51e9bdef7ca5776a255c41fe85
Aafiya <aafiya.hussain2@gmail.com>
2021-01-15 15:58:38 +0530

    added whitenoise to middleware

--- Classroom_project/settings.py
+++ Classroom_project/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Classroom_project.urls'
